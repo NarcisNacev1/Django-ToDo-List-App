292241d9cd93edd143a1ad370b5ad68b31309e5b
Narcis <narcis.karanfilov@gmail.com>
2024-09-28 22:22:22 +0200

    Worked on templates and adding tasks to the todoList in django

--- djangoproject/djangoapp/views.py
+++ djangoproject/djangoapp/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList,Item
 
-def index(response, name):
-    ls = ToDoList.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "djangoapp/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "djangoapp/home.html", {})
 
