30285b83334c3f7df378a6136828978e632af4b5
Narcis <narcis.karanfilov@gmail.com>
2024-10-03 00:29:16 +0200

    Added register site so you can register yourself and used crispy-forms

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'djangoapp.apps.DjangoappConfig',
+    'register.apps.RegisterConfig'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("djangoapp.urls")),
+    path("home", include("djangoapp.urls")),
+    path("register", v.register, name='register'),
 ]
